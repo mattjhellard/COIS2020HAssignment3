14d40ca98af5dd1076eaa2bb609ada1936158660
colemiller123 <114594477+colemiller123@users.noreply.github.com>
2022-11-21 12:06:16 -0500

    Working on checks for AddFile()

--- Assignment3.cs
+++ Assignment3.cs
@@ -5,6 +5,9 @@
  */
 
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
 public class FileSystem
 {
     private class Node
@@ -38,17 +41,68 @@ public class FileSystem
     // Returns false if the file already exists at that address or the path is undefined; true otherwise
     public bool AddFile(string address) 
     {
-        //CHeck proper address given
-
+        //Check proper address given
+        //Format: "/DirectoryName/DirectoryName/.../Filename"
         
+        //Check if the path is null
+        if (address != null)
+        {
+            //Valid
+            Console.WriteLine("valid");
+        }
+        else
+        {
+            //Invalid file path
+            Console.WriteLine("String can not be null.");
+        }
+
+        //Check if the path starts at the root
+        if (Path.IsPathRooted(address))
+        {
+            //Valid
+            Console.WriteLine("valid");
+        }
+        else {
+            //Invalid file path
+            Console.WriteLine("Please enter a valid file path in the form: /Directory/.../Filename");
+        }
+
+        //Check if the path has a valid extension eg .txt
+        if (Path.HasExtension(address))
+        {
+            //Valid
+            Console.WriteLine("valid2");
+        }
+        else
+        {
+            //No extension
+            Console.WriteLine("Please add a file extension. Example: .txt or .exe");
+        }
+
+        //Check if the path contains invalid characters
+        if (1 == 2)
+        {
+            //Valid
+            Console.WriteLine("valid2");
+        }
+        else
+        {
+            //No extension
+            Console.WriteLine("Please add a file extension. Example: .txt or .exe");
+        }
+
+        //CHeck if its an absolute path
+
+
+
         //navigate to this directory
 
         //CHeck if their are conflicts
 
         //if no conflicts add file
-        
-        
-        
+
+
+
         return false; //placeholder, replace with real code
     }
 
@@ -109,5 +163,6 @@ public class Demo
         FileSystem testFileSystem = new FileSystem();
 
         testFileSystem.PrintFileSystem();
+        testFileSystem.AddFile(null);
     }
 }
